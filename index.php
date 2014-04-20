25e4db4457d5906553b23d9d9532bbdda2c0b02f
Robert McElaney <robert@ctrlaltdesign.com>
2014-04-20 10:21:54 -0400

    added styles

--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 <html>
 <head>
 <title> Ctrl Alt Design Lab</title>
+
 <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 
 <body>
 
 
+<!--
+Common Libraries
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js"></script>
+<script src="http://code.createjs.com/createjs-2013.12.12.min.js"></script>
+-->
 
 <script src="js/script.js"></script>
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 
 
+window.onload = function(){
+var ww = window.innerWidth,
+	wh = window.innerHeight,
+	cvs = document.getElementById('can001'),
+	ctx = cvs.getContext("2D");
+	
+	var sqr = ww < wh? ww : wh;
+    
+	w = cvs.width =	h = cvs.height = sqr;
+	cvs.style.marginLeft = (sqr/2)*-1+"px";
 
+}
