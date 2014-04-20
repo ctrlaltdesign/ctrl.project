a1d21eaa6c51e7a0d32cdaae58ae9b9551e6041b
Robert McElaney <robert@ctrlaltdesign.com>
2014-04-20 10:15:58 -0400

    structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Ctrl Alt Design Lab</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+
+
+<script src="js/script.js"></script>
+
+<?php include 'inc/ganalytics.php' ?>
+
+</body>
+</html>
